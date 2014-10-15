387c6390405cdb11b6cd32326892acd38e5e1b7d
Chris Holden <ceholden@gmail.com>
2014-10-15 11:01:12 -0400

    Reorganized config parsing to better utilize configparser API

--- yatsm/config_parser.py
+++ yatsm/config_parser.py
@@ -22,18 +22,18 @@ def _parse_config_v_zero_pt_one(config_file):
     config.read(config_file)
 
     # Configuration for dataset
-    dataset_config = dict.fromkeys(['input_file', 'date_format',
-                                    'output', 'output_prefix',
-                                    'n_bands', 'mask_band',
-                                    'green_band', 'swir1_band',
-                                    'use_bip_reader'])
-    for k in dataset_config:
-        dataset_config[k] = config.get('dataset', k)
+    dataset_config = {}
 
-    dataset_config['n_bands'] = int(dataset_config['n_bands'])
-    dataset_config['mask_band'] = int(dataset_config['mask_band']) - 1
-    dataset_config['green_band'] = int(dataset_config['green_band']) - 1
-    dataset_config['swir1_band'] = int(dataset_config['swir1_band']) - 1
+    dataset_config['input_file'] = config.get('dataset', 'input_file')
+    dataset_config['date_format'] = config.get('dataset', 'date_format')
+    dataset_config['output'] = config.get('dataset', 'output')
+    dataset_config['output_prefix'] = config.get('dataset', 'output_prefix')
+    dataset_config['n_bands'] = config.getint('dataset', 'n_bands')
+    dataset_config['mask_band'] = config.getint('dataset', 'mask_band') - 1
+    dataset_config['green_band'] = config.getint('dataset', 'green_band') - 1
+    dataset_config['swir1_band'] = config.getint('dataset', 'swir1_band') - 1
+    dataset_config['use_bip_reader'] = config.getboolean(
+        'dataset', 'use_bip_reader')
 
     # Configuration for training and classification
     dataset_config['training_image'] = config.get('classification',
@@ -49,10 +49,10 @@ def _parse_config_v_zero_pt_one(config_file):
     # Configuration for YATSM algorithm
     yatsm_config = {}
 
-    yatsm_config['consecutive'] = int(config.get('YATSM', 'consecutive'))
-    yatsm_config['threshold'] = float(config.get('YATSM', 'threshold'))
-    yatsm_config['min_obs'] = int(config.get('YATSM', 'min_obs'))
-    yatsm_config['min_rmse'] = float(config.get('YATSM', 'min_rmse'))
+    yatsm_config['consecutive'] = config.getint('YATSM', 'consecutive')
+    yatsm_config['threshold'] = config.getfloat('YATSM', 'threshold')
+    yatsm_config['min_obs'] = config.getint('YATSM', 'min_obs')
+    yatsm_config['min_rmse'] = config.getfloat('YATSM', 'min_rmse')
     yatsm_config['freq'] = config.get(
         'YATSM', 'freq').replace(',', ' ').split(' ')
     yatsm_config['freq'] = [int(v) for v in yatsm_config['freq']
@@ -63,9 +63,9 @@ def _parse_config_v_zero_pt_one(config_file):
                                              yatsm_config['test_indices']
                                              if b != ''])
     yatsm_config['screening'] = config.get('YATSM', 'screening')
-    yatsm_config['lassocv'] = config.get('YATSM', 'lassocv').lower() == 'true'
-    yatsm_config['reverse'] = config.get('YATSM', 'reverse').lower() == 'true'
-    yatsm_config['robust'] = config.get('YATSM', 'robust').lower() == 'true'
+    yatsm_config['lassocv'] = config.getboolean('YATSM', 'lassocv')
+    yatsm_config['reverse'] = config.getboolean('YATSM', 'reverse')
+    yatsm_config['robust'] = config.getboolean('YATSM', 'robust')
 
     return (dataset_config, yatsm_config)
 
