be194e2ee5e597dedd464a0ec7397aee246c9fe8
Chris Holden <ceholden@gmail.com>
2014-10-23 09:59:42 -0400

    work on spatially aware crossvalidation

--- yatsm/classifiers/__init__.py
+++ yatsm/classifiers/__init__.py
@@ -9,6 +9,9 @@ except ImportError:
     import configparser
 import logging
 
+import numpy as np
+import scipy.ndimage
+
 from random_forest_helper import RandomForestHelper
 
 logger = logging.getLogger(__name__)
@@ -24,7 +27,7 @@ class AlgorithmNotFoundException(Exception):
 
 
 def ini_to_algorthm(config_file):
-    """ Returns instance of classification algorithm helper from config file
+    """ Return instance of classification algorithm helper from config file
 
     Args:
       config_file (str): location of configuration file for algorithm
@@ -52,3 +55,42 @@ def ini_to_algorthm(config_file):
 
     # Return instance of algorithm
     return algo(config)
+
+
+def spatial_crossvalidate(roi, mask_values=[0], n_folds=3):
+    """ Return crossvalidated accuracy for spatial training data
+
+    Training data samples physically located next to test samples are likely to
+    be strongly related due to spatial autocorrelation. This violation of
+    independence will artificially inflate crossvalidated measures of
+    algorithm performance. This function will performance crossvalidation by
+    treating contiguous training data samples as one sample during
+    crossvalidation.
+
+    Args:
+      roi (np.ndarray): training data raster to sample from
+      mask_values (np.ndarray, optional): mask values to ignore from raster
+      n_folds (int, optional): number of folds to use
+
+    Returns:
+
+    """
+    if isinstance(mask_values, int):
+        mask_values = [mask_values]
+
+    mask = np.logical_or.reduce([roi == mv for mv in mask_values])
+
+    labeled, n_labels = scipy.ndimage.label(roi)
+
+    labels = np.unique(labeled)
+    unmasked_labels = np.in1d(labels, np.unique(labeled[~mask]))
+    labels = labels[unmasked_labels]
+
+
+
+
+
+
+
+
+
