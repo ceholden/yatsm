017a3906d75790504c912dc8d36004952a248616
Chris Holden <ceholden@gmail.com>
2014-10-29 22:04:27 -0400

    Oops - fixed finding of yatsm when not installed as module

--- scripts/run_yatsm.py
+++ scripts/run_yatsm.py
@@ -61,7 +61,8 @@ try:
 except ImportError:
     # Try adding `pwd` to PYTHONPATH
     import sys
-    sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+    sys.path.append(os.path.dirname(os.path.dirname(
+        os.path.abspath(__file__))))
     from yatsm.yatsm import YATSM, make_X
     from yatsm.ts_driver.timeseries_ccdc import CCDCTimeSeries
 
