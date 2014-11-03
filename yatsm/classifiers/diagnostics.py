df780ab6ef786ced9a467d930395ad91f6fbb7a7
Chris Holden <ceholden@gmail.com>
2014-11-02 22:46:21 -0500

    Added new spatial KFold for y data identified by row/col location

--- yatsm/classifiers/diagnostics.py
+++ yatsm/classifiers/diagnostics.py
@@ -9,8 +9,93 @@ from sklearn.utils import check_random_state
 logger = logging.getLogger('yatsm')
 
 
-class KFoldROI(object):
+class SpatialKFold(object):
     """ Spatial cross validation iterator
+
+    Training data samples physically located next to test samples are likely to
+    be strongly related due to spatial autocorrelation. This violation of
+    independence will artificially inflate crossvalidated measures of
+    algorithm performance.
+
+    Provides training and testing indices to split data into training and
+    testing sets. Splits a "Region of Interest" image into k consecutive
+    folds. Each fold is used as a validation set once while k - 1 remaining
+    folds form the training set.
+
+    Parameters:
+      y (np.ndarray): Labeled features
+
+      row (np.ndarray): Row (y) pixel location for each `y`
+
+      col (np.ndarray): Column (x) pixel location for each `x`
+
+      n_folds (int, optional): Number of folds (default: 3)
+
+      shuffle (bool, optional): Shuffle the unique training data regions before
+        splitting into batches (default: False)
+
+      random_state (None, int, or np.random.RandomState): Pseudo-random number
+        generator to use for random sampling. If None, default to numpy RNG
+        for shuffling
+
+    """
+
+    shuffle = False
+
+    def __init__(self, y, row, col, n_folds=3, shuffle=False,
+                 random_state=None):
+        self.y = y
+        self.row = row
+        self.col = col
+        self.n_folds = n_folds
+
+        if shuffle:
+            self.shuffle = True
+            self.rng = check_random_state(random_state)
+
+        self._recreate_labels()
+
+    def __iter__(self):
+        fold_sizes = (self.n // self.n_folds) * np.ones(self.n_folds,
+                                                        dtype=np.int)
+        fold_sizes[:self.n % self.n_folds] += 1
+        current = 0
+
+        ind = np.arange(self.y.size)
+
+        for fold_size in fold_sizes:
+            start, stop = current, current + fold_size
+
+            test_i = self._labels_to_indices(self.labels[start:stop])
+
+            yield ind[test_i], ind[~test_i]
+            current = stop
+
+    def _recreate_labels(self):
+        """ Internal method to label regions of `self.y` from pixel locations
+        """
+        roi = np.zeros((self.row.max() + 1, self.col.max() + 1),
+                       dtype=self.y.dtype)
+        roi[self.row, self.col] = self.y
+
+        self.labeled, _ = scipy.ndimage.label(roi)
+        self.labels = np.unique(self.labeled[self.labeled != 0])
+        self.n = self.labels.size
+
+        if self.shuffle:
+            self.rng.shuffle(self.labels)
+
+        self.indices = []
+
+    def _labels_to_indices(self, labels):
+        lab_row, lab_col = np.where(np.in1d(
+            self.labeled, labels).reshape(self.labeled.shape))
+        return np.logical_and(np.in1d(self.row, lab_row),
+                              np.in1d(self.col, lab_col))
+
+
+class SpatialKFold_ROI(object):
+    """ Spatial cross validation iterator on ROI images
 
     Training data samples physically located next to test samples are likely to
     be strongly related due to spatial autocorrelation. This violation of
