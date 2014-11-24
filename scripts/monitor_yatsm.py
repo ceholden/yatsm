e411edf21bc13518fb1dba8f394fb4d0ed7ed60d
Chris Holden <ceholden@gmail.com>
2014-11-23 23:16:56 -0500

    Add prototype CLI to monitoring script

---
+++ scripts/monitor_yatsm.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+""" Near real-time monitoring
+
+Usage:
+    monitor_yatsm.py [options] <config_file> <job_number> <total_jobs>
+
+Options:
+    --resume                    Do not overwrite pre-existing results
+    -v --verbose                Show verbose debugging messages
+    --version                   Print program version and exit
+    -h --help                   Show help
+
+"""
