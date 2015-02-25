ceholden/yatsm

commit 0dfa1d6e2328f8ef15ad8f7229eabadc10140b53

Add values within mask_band to mask from dataset analysis. Close #16

scripts/line_yatsm.py
@@ -243,10 +243,11 @@ def run_pixel(X, Y, dataset_config, yatsm_config, px=0, py=0):
         raise TSLengthException('Not enough valid observations')
 
     # Otherwise continue
-    clear = np.logical_and.reduce([Y[mask_band, :] <= 1,
-                                   np.all(Y[:mask_band, :] > 0, axis=0),
-                                   np.all(Y[:mask_band, :] < 10000, axis=0)
-                                   ])
+    clear = (
+        np.in1d(Y[mask_band, :], dataset_config['mask_values']) *
+        np.all(Y[:mask_band, :] > 0, axis=0) *
+        np.all(Y[:mask_band, :] < 10000, axis=0)
+    )
 
     Y = Y[:mask_band, clear]
     X = X[clear, :]

examples/example.ini
@@ -21,6 +21,8 @@ output_prefix = yatsm_r
 n_bands = 8
 # Mask band (e.g., Fmask)
 mask_band = 8
+# Values to mask within the mask band
+mask_values = 2, 3, 4, 255
 # Indices for multi-temporal cloud masking (indexed on 1)
 green_band = 2
 swir1_band = 5

examples/p035r032_example.ini
@@ -21,6 +21,8 @@ output_prefix = yatsm_r
 n_bands = 8
 # Mask band (e.g., Fmask)
 mask_band = 8
+# Values to mask within the mask band
+mask_values = 2, 3, 4, 255
 # Indices for multi-temporal cloud masking (indexed on 1)
 green_band = 2
 swir1_band = 5

yatsm/config_parser.py
@@ -107,6 +107,8 @@ def parse_config_v0_2_x(config_file):
     dataset_config, yatsm_config = parse_config_v0_1_x(config_file)
 
     defaults = """
+[dataset]
+mask_values = 2, 3, 4, 255
 [YATSM]
 remove_noise = True
 dynamic_rmse = False
@@ -116,6 +118,10 @@ commission_alpha =
     config = configparser.ConfigParser(allow_no_value=True)
     config.readfp(StringIO.StringIO(defaults))
     config.read(config_file)
+
+    mask_values = config.get('dataset', 'mask_values')
+    dataset_config['mask_values'] = [
+        int(mv) for mv in mask_values.replace(',', ' ').split(' ') if mv != '']
 
     yatsm_config['remove_noise'] = config.getboolean('YATSM', 'remove_noise')
     yatsm_config['dynamic_rmse'] = config.getboolean('YATSM', 'dynamic_rmse')

commit e49fd869417db40c7c037c28e4ca8678b1436186

Fix horrible bug in masking introduced in 0dfa1d6
Mask values were being retained instead of removed

scripts/line_yatsm.py
@@ -248,7 +248,7 @@ def run_pixel(X, Y, dataset_config, yatsm_config, px=0, py=0):
 
     # Otherwise continue
     clear = (
-        np.in1d(Y[mask_band, :], dataset_config['mask_values']) *
+        ~np.in1d(Y[mask_band, :], dataset_config['mask_values']) *
         np.all(Y[:mask_band, :] > 0, axis=0) *
         np.all(Y[:mask_band, :] < 10000, axis=0)
     )