c13c254cc61e81984d0ce2137d0227d7112c7f06
Chris Holden <ceholden@gmail.com>
2015-06-25 15:47:26 -0400

    Update segmentation script with notes and arg changes

--- sandbox/segment/make_segment.sh
+++ sandbox/segment/make_segment.sh
@@ -31,8 +31,12 @@ yatsm_map.py -v \
 # gdal_translate -of ENVI -co "INTERLEAVE=BIP" \
 #     p035r032_2010-06-01_bgw_uint8_predict.gtif temp_bip
 
+# TRY A SMALL SEGMENT TOLERANCE -- skip first phase and then go to the region size restriction auxiliary merging
+# If you stretch the all of the data, you're essentially treating all bands the same
+#
+
 ~/Documents/segment/segment/bin/segment \
-    -t 50 -m 0.1 -n 11,11,25,50,50 -8 -o bgw_seg \
+    -t 50 -m 0.1 -n 11,11,25,50,250 -8 -o bgw_seg \
     p035r032_2010-06-01_bgw_uint8_predict.gtif
 
 armap=$(ls bgw_seg.armap.*[0-9])
