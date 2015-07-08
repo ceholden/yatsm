2ce37961a5f7eb2fb9e92c78cc3cfbec77ab0a33
Chris Holden <ceholden@gmail.com>
2015-07-08 15:05:10 -0500

    Fix duplicate logger in mapping script

--- scripts/yatsm_map.py
+++ scripts/yatsm_map.py
@@ -74,8 +74,6 @@ from yatsm.regression.transforms import harm
 gdal.UseExceptions()
 gdal.AllRegister()
 
-FORMAT = '%(asctime)s:%(levelname)s:%(module)s.%(funcName)s:%(message)s'
-logging.basicConfig(format=FORMAT, level=logging.INFO, datefmt='%H:%M:%S')
 logger = logging.getLogger('yatsm')
 
 # QA/QC values for segment types
