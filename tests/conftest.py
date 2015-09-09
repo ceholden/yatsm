987af4c16605226af55cae08cdcf829d72fd7f45
Chris Holden <ceholden@gmail.com>
2015-09-09 00:32:52 -0400

    Refactor cache tests; more to go

---
+++ tests/conftest.py
@@ -0,0 +1,83 @@
+import fnmatch
+from functools import partial
+import os
+import shutil
+import tarfile
+from tempfile import mkdtemp
+
+try:
+    from os import walk
+except ImportError:
+    from scandir import walk
+
+import numpy as np
+import pytest
+
+here = os.path.dirname(__file__)
+example_cachedir = os.path.join(here, 'data', 'cache')
+example_testfile = os.path.join(example_cachedir, 'yatsm_r0_n447_b8.npy.npz')
+
+
+# EXAMPLE DATASETS
+@pytest.fixture(scope='session')
+def example_timeseries(request):
+    """ Extract example timeseries returning paths & image IDs
+    """
+    path = mkdtemp('_yatsm')
+    tgz = os.path.join(here, 'data', 'p035r032_subset.tar.gz')
+    with tarfile.open(tgz) as tgz:
+        tgz.extractall(path)
+    print(path)
+    request.addfinalizer(partial(shutil.rmtree, path))
+
+    subset_path = os.path.join(path, 'subset')
+    stack_images, stack_image_IDs = [], []
+    for root, dnames, fnames in walk(subset_path):
+        for fname in fnmatch.filter(fnames, 'L*stack'):
+            stack_images.append(os.path.join(root, fname))
+            stack_image_IDs.append(os.path.basename(root))
+
+    return subset_path, np.asarray(stack_images), np.asarray(stack_image_IDs)
+
+
+@pytest.fixture(scope='session')
+def example_data(request):
+    return np.load(example_testfile)
+
+
+# EXAMPLE CACHE DATA
+@pytest.fixture(scope='function')
+def cachedir(request):
+    return example_cachedir
+
+
+@pytest.fixture(scope='function')
+def cachefile(request):
+    return example_testfile
+
+
+# MISC
+@pytest.fixture(scope='function')
+def mkdir_permissions(request):
+    """ Fixture for creating dir with specific read/write permissions """
+    def make_mkdir(read=False, write=False):
+        if read and write:
+            mode = 0755
+        elif read and not write:
+            mode = 0555
+        elif not read and write:
+            mode = 0333
+        elif not read and not write:
+            mode = 0000
+
+        path = mkdtemp()
+        os.chmod(path, mode)
+
+        def fin():
+            os.chmod(path, 0755)
+            os.removedirs(path)
+        request.addfinalizer(fin)
+
+        return path
+
+    return make_mkdir
---
+++ tests/test_cache.py
@@ -0,0 +1,145 @@
+""" Tests for yatsm.cache
+"""
+import os
+import tempfile
+
+import numpy as np
+import pytest
+from yatsm import cache, reader
+
+cache_params = pytest.mark.parametrize('n_images,n_row,n_bands', [(447, 0, 8)])
+
+
+@cache_params
+def test_get_line_cache_name(cachedir, cachefile, n_images, n_row, n_bands):
+    cfg = dict(cache_line_dir=cachedir)
+    assert cachefile == cache.get_line_cache_name(cfg,
+                                                  n_images, n_row, n_bands)
+
+
+@cache_params
+def test_get_line_cache_pattern_glob(cachedir, cachefile,
+                                     n_images, n_row, n_bands):
+    import glob
+    pattern = cache.get_line_cache_pattern(n_row, n_bands, regex=False)
+    found = glob.glob('%s/%s' % (cachedir, pattern))[0]
+
+    assert found == cachefile
+
+
+@cache_params
+def test_get_line_cache_pattern_regex(cachedir, cachefile,
+                                      n_images, n_row, n_bands):
+    import re
+    pattern = cache.get_line_cache_pattern(n_row, n_bands, regex=True)
+
+    found = [f for f in os.listdir(cachedir) if re.match(pattern, f)]
+    found = os.path.join(cachedir, found[0])
+
+    assert found == cachefile
+
+
+def test_test_cache(mkdir_permissions):
+    # Test when cache dir exists already
+    path = mkdir_permissions(read=False, write=False)
+    assert (False, False) == cache.test_cache(dict(cache_line_dir=path))
+
+    path = mkdir_permissions(read=False, write=True)
+    assert (False, True) == cache.test_cache(dict(cache_line_dir=path))
+
+    path = mkdir_permissions(read=True, write=False)
+    assert (True, False) == cache.test_cache(dict(cache_line_dir=path))
+
+    path = mkdir_permissions(read=True, write=True)
+    assert (True, True) == cache.test_cache(dict(cache_line_dir=path))
+
+    # Test when cache dir doesn't exist
+    tmp = os.path.join(tempfile.tempdir,
+                       next(tempfile._get_candidate_names()) + '_yatsm')
+    read_write = cache.test_cache(dict(cache_line_dir=tmp))
+    os.removedirs(tmp)
+
+    assert (True, True) == read_write
+
+
+def test_read_cache_file(cachefile, example_data):
+    assert None is cache.read_cache_file('asdf')
+
+    np.testing.assert_equal(example_data['Y'],
+                            cache.read_cache_file(cachefile))
+
+
+def test_read_cache_file_imageIDs(cachefile, example_data):
+    image_IDs = example_data['image_IDs']
+    # Expect None since image IDs won't match
+    assert None is cache.read_cache_file(cachefile,
+                                         image_IDs[::-1])
+
+    np.testing.assert_equal(example_data['Y'],
+                            cache.read_cache_file(cachefile, image_IDs))
+
+
+def test_write_cache_file(cachefile, example_data):
+    cache.write_cache_file('test.npz',
+                           example_data['Y'], example_data['image_IDs'])
+    test = np.load('test.npz')
+    Y, image_IDs = test['Y'], test['image_IDs']
+    os.remove('test.npz')
+
+    np.testing.assert_equal(Y, example_data['Y'])
+    np.testing.assert_equal(image_IDs, example_data['image_IDs'])
+
+
+def test_update_cache_file_delete_obs(cachefile, example_data):
+    choice = np.random.choice(example_data['image_IDs'].size,
+                              size=100, replace=False)
+    new_Y = example_data['Y'][:, choice, :]
+    new_image_IDs = example_data['image_IDs'][choice]
+
+    # For now, just use image_IDs as `images` since we won't be updating
+    # from images
+    cache.update_cache_file(new_image_IDs, new_image_IDs,
+                            cachefile,
+                            'test.npz',
+                            0, reader.read_row_GDAL)
+    test = np.load('test.npz')
+    Y, image_IDs = test['Y'], test['image_IDs']
+    os.remove('test.npz')
+
+    np.testing.assert_equal(new_Y, Y)
+    np.testing.assert_equal(new_image_IDs, image_IDs)
+
+
+def test_update_cache_file_add_obs(cachefile, example_data,
+                                   example_timeseries):
+    """ Grab a subset of test data and see if we get more data back """
+    path, stack_images, stack_image_IDs = example_timeseries
+    # Presort and subset for comparison
+    sort_idx = np.argsort(example_data['image_IDs'])
+    test_Y = example_data['Y'][:, sort_idx, :]
+    test_IDs = example_data['image_IDs'][sort_idx]
+
+    size_1 = 100
+    size_2 = 200
+
+    sort_idx = np.argsort(stack_image_IDs)[:size_2]
+    stack_images = stack_images[sort_idx]
+    stack_IDs = stack_image_IDs[sort_idx]
+
+    # Create reduced dataset to add to
+    np.savez_compressed('test.npz',
+                        Y=test_Y[:, :size_1, :],
+                        image_IDs=test_IDs[:size_1])
+
+    # Write update and read back
+    cache.update_cache_file(stack_images, stack_IDs,
+                            'test.npz', 'test_new.npz',
+                            0, reader.read_row_GDAL)
+    updated = np.load('test_new.npz')
+
+    # Test and clean update
+    np.testing.assert_equal(test_Y[:, :size_2, :], updated['Y'])
+    np.testing.assert_equal(test_IDs[:size_2], updated['image_IDs'])
+
+    os.remove('test.npz')
+    os.remove('test_new.npz')
