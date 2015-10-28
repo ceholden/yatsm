f2b63a9edc0bea97d987e341a51279a6b9d97db4
Chris Holden <ceholden@gmail.com>
2015-10-28 16:16:57 -0400

    Include lineno in logger format

--- yatsm/log_yatsm.py
+++ yatsm/log_yatsm.py
@@ -1,6 +1,6 @@
 import logging
 
-_FORMAT = '%(asctime)s:%(levelname)s:%(module)s.%(funcName)s:%(message)s'
+_FORMAT = '%(asctime)s:%(levelname)s:%(lineno)s:%(module)s.%(funcName)s:%(message)s'
 _formatter = logging.Formatter(_FORMAT, '%H:%M:%S')
 _handler = logging.StreamHandler()
 _handler.setFormatter(_formatter)
