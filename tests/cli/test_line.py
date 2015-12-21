aa4c673d353de07d29d905788f8b7e7ec508b081
Chris Holden <ceholden@gmail.com>
2015-12-21 00:30:16 -0500

    Only run 1 row of new dataset

--- tests/cli/test_line.py
+++ tests/cli/test_line.py
@@ -9,5 +9,5 @@ def test_cli_line_pass_1(example_timeseries):
     """ Run correctly, with 1 of 1 jobs
     """
     runner = CliRunner()
-    result = runner.invoke(line.line, [example_timeseries['config'], '1', '1'])
+    result = runner.invoke(line.line, [example_timeseries['config'], '1', '5'])
     assert result.exit_code == 0
