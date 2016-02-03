c122b7dfa358e6a506a0f224cc9182d1833216b0
Chris Holden <ceholden@gmail.com>
2016-02-02 22:59:12 -0500

    Fix #79; add test coverage

--- tests/algorithms/test_postprocess.py
+++ tests/algorithms/test_postprocess.py
@@ -57,3 +57,54 @@ def test_refit_nochange_rlm(sim_nochange):
     rmse = np.array([0.96866816, 0.96866816])
     np.testing.assert_allclose(refit[0]['rlm_coef'], coef)
     np.testing.assert_allclose(refit[0]['rlm_rmse'], rmse)
+
+
+def test_refit_nochange_reg(sim_nochange):
+    """ Test refit ``keep_regularized=False`` (i.e., not ignoring coef == 0)
+    """
+    from sklearn.linear_model import LinearRegression as OLS
+    estimator = OLS()
+
+    refit = refit_record(sim_nochange, 'ols', estimator,
+                         keep_regularized=False)
+    assert 'ols_coef' in refit.dtype.names
+    assert 'ols_rmse' in refit.dtype.names
+
+    coef = np.array([[-3.83016528e+03, -3.83016528e+03],
+                     [5.24635240e-03, 5.24635240e-03]])
+    rmse = np.array([0.96794599, 0.96794599])
+    np.testing.assert_allclose(refit[0]['ols_coef'], coef)
+    np.testing.assert_allclose(refit[0]['ols_rmse'], rmse)
+
+
+def test_refit_none():
+    """ Test refit if model is None/[]
+    """
+    refit = refit_record(None, 'ols', None)
+    assert refit is None
+    refit = refit_record([], 'ols', None)
+    assert refit is None
+
+
+# ISSUE #79
+def test_refit_issue_79(sim_nochange):
+    """ Issue 79: missing coverage for case when record['coef'] are all zero
+
+    Fix is to use ``refit_[(coef|rmse)]`` prefix variable to index the record
+    name
+    """
+    from yatsm.regression import RLM
+    estimator = RLM(maxiter=10)
+
+    # Set record.coef to 0.
+    sim_nochange.record['coef'] = np.zeros_like(sim_nochange.record['coef'])
+
+    refit = refit_record(sim_nochange, 'rlm', estimator,
+                         keep_regularized=True)
+    assert 'rlm_coef' in refit.dtype.names
+    assert 'rlm_rmse' in refit.dtype.names
+
+    coef = np.zeros_like(sim_nochange.record[0]['coef'])
+    rmse = np.array([0.97117668, 0.97117668])
+    np.testing.assert_allclose(refit[0]['rlm_coef'], coef)
+    np.testing.assert_allclose(refit[0]['rlm_rmse'], rmse)
