3c1a9dfcbfb1934fa2c48c5828bd60808f46a6d5
Chris Holden <ceholden@gmail.com>
2016-05-19 10:10:14 -0400

    Implement GDAL reader within io

--- yatsm/io/_gdal.py
+++ yatsm/io/_gdal.py
@@ -1,9 +1,15 @@
 """ Tools related to reading time series data using GDAL / rasterio
 """
 from functools import partial
+import logging
 import os
 
+import numpy as np
 import pandas as pd
+import rasterio
+import xarray as xr
+
+logger = logging.getLogger(__name__)
 
 
 def parse_dataset_file(input_file, date_format):
@@ -17,22 +23,166 @@ def parse_dataset_file(input_file, date_format):
         pd.DataFrame: Dataset information
     """
     dt_parser = lambda x: pd.datetime.strptime(x, date_format)
+
     df = pd.read_csv(input_file,
                      parse_dates=['date'], date_parser=dt_parser)
+    df.set_index('date', inplace=True, drop=False)
+    df.index.name = 'time'
+
+    df.sort_values('date', inplace=True)
+
     if not os.path.isabs(df['filename'][0]):
         _root = os.path.abspath(os.path.dirname(input_file))
-        _root_join = partial(os.path.join, _root)
-        df['filename'] = map(_root_join, df['filename'])
+        df['filename'] = map(partial(os.path.join, _root), df['filename'])
 
     return df
 
 
-def get_coordinates(ul, res, window):
-    """ Return Y/X coordinates of a raster to pass to xarray
+class GDALDataset(object):
+    """ A time series that can be read in by GDAL
 
     Args:
-        ul (sequence[float, float]): Upper left Y/X coordinates
-        res (sequence[float, float]): Pixel resolution Y/X
-        window (): Number of
+        df (pd.DataFrame): A Pandas dataframe describing time series (requires
+            keys 'filename' and 'date')
+        input_file (str): If `df` is not specified, read time series dataset
+            information from this file
+        date_format (str): If `df` is not specified, parse date column in
+            `input_file` with this date string format
+        band_names (list[str]): Names of all  bands in this time series
+        keep_open (bool): Keep ``rasterio`` file descriptors open once opened?
+
     """
-    pass
+    def __init__(self, df=None, input_file='', date_format='%Y%m%d',
+                 band_names=None,
+                 keep_open=False, **kwargs):
+        if isinstance(df, pd.DataFrame):
+            if not all([k in df.keys() for k in ('date', 'filename')]):
+                raise KeyError('pd.DataFrame passed should contain "date" and '
+                               '"filename" keys')
+            self.df = df
+        elif input_file and date_format:
+            self.df = parse_dataset_file(input_file, date_format)
+        else:
+            raise ValueError('Must specify either a pd.DataFrame or both'
+                             '"input_file" and "date_format" arguments')
+        self.keep_open = keep_open
+        self.band_names = band_names
+
+        self._init_attrs_from_file(self.df['filename'][0])
+
+    def _init_attrs_from_file(self, filename):
+        with rasterio.drivers():
+            with rasterio.open(filename, 'r') as src:
+                #: dict: rasterio metadata of first file
+                self.md = src.meta.copy()
+                self.crs = src.crs
+                self.affine = src.affine
+                self.res = src.res
+                self.ul = src.ul(0, 0)
+                self.nrow = src.height
+                self.ncol = src.width
+                self.nobs = len(self.df)
+                # We only use one datatype for reading -- promote to largest
+                # if hetereogeneous
+                self.dtype = src.dtypes[0]
+                if not all([dt == self.dtype for dt in src.dtypes[1:]]):
+                    logger.warning('GDAL reader cannot read multiple data '
+                                   'types. Promoting memory allocation to '
+                                   'largest datatype of source bands')
+                    for dtype in src.dtypes[1:]:
+                        self.dtype = np.promote_types(self.dtype, dtype)
+                # TODO: block shape?
+
+    @property
+    def time(self):
+        return self.df['date']
+
+    @property
+    def _src(self):
+        """ An optionally memoized generator on time series datasets
+        """
+        if self.keep_open:
+            if not hasattr(self, '_src_open'):
+                with rasterio.drivers():
+                    self._src_open = [rasterio.open(f, 'r') for
+                                      f in self.df['filename']]
+            for _src in self._src_open:
+                yield _src
+        else:
+            with rasterio.drivers():
+                for f in self.df['filename']:
+                    yield rasterio.open(f, 'r')
+
+    def get_blocks(self):
+        # TODO: return reader's best guess at what blocks should be
+        raise NotImplementedError
+
+    def read(self, window=None, out=None):
+        """ Read time series, usually inside of a specified window
+
+        Args:
+            window (tuple): A pair (tuple) of pairs of ints specifying
+                the start and stop indices of the window rows and columns
+            out (np.ndarray): A NumPy array of pre-allocated memory to read
+                the time series into. Its shape should be:
+
+                (time series length, # bands, # rows, columns)
+
+        Returns:
+            np.ndarray: A NumPy array containing the time series data
+        """
+        # Parse geographic/projected coordinates from window query
+        ((row_min, row_max), (col_min, col_max)) = window
+        x_min, y_min = (col_min, row_max) * self.affine
+        x_max, y_max = (col_max, row_min) * self.affine
+        coord_bounds = (x_min, y_min, x_max, y_max)
+
+        # TODO: check `out` is compatible if provided by user
+        shape = (self.nobs, len(self.band_names), window[0][1], window[1][1])
+        out = (out if isinstance(out, np.ndarray)
+               else np.empty((shape), dtype=self.dtype))
+
+        for idx, src in enumerate(self._src):
+            _window = src.window(*coord_bounds, boundless=True)
+            src.read(window=_window, out=out[idx, ...], boundless=True)
+
+        return out
+
+    def read_dataarray(self, window=None, out=None):
+        """ Read time series, usually inside of a window, as xarray.DataArray
+
+        Args:
+            window (tuple): A pair (tuple) of pairs of ints specifying
+                the start and stop indices of the window rows and columns
+            out (np.ndarray): A NumPy array of pre-allocated memory to read
+                the time series into. Its shape should be:
+
+                (time series length, # bands, # rows, columns)
+
+        Returns:
+            xarray.DataArray: A DataArray containing the time series data with
+                coordinate dimenisons (time, band, y, and x)
+        """
+        values = self.read(window=window, out=out)
+        coords_y, coords_x = self.window_coords(window)
+        da = xr.DataArray(
+            values,
+            dims=['time', 'band', 'y', 'x'],
+            coords=[self.df['date'], self.band_names, coords_y, coords_x]
+        )
+        return da
+
+    def window_coords(self, window):
+        """ Return Y/X coordinates of a raster to pass to xarray
+
+        Args:
+            window (tuple): Window to read from ((ymin, ymax), (xmin, xmax)) in
+                pixel space
+
+        Returns:
+            tuple (np.ndarray, np.ndarray): Y and X coordinates for window
+        """
+        coord_y = self.ul[0] + self.res[0] * np.arange(*window[0])
+        coord_x = self.ul[1] + self.res[1] * np.arange(*window[1])
+
+        return (coord_y, coord_x)
--- yatsm/io/_xarray.py
+++ yatsm/io/_xarray.py
@@ -9,6 +9,10 @@ from . import _gdal
 
 logger = logging.getLogger(__name__)
 
+READER_BACKENDS = {
+    'GDAL': _gdal.GDALDataset
+}
+
 
 def apply_band_mask(arr, mask_band, mask_values):
     """ Mask all `bands` in `arr` based on some mask values in a band
@@ -48,7 +52,7 @@ def apply_range_mask(arr, min_values, max_values):
     return arr.where((arr >= mins) & (arr <= maxs))
 
 
-def read_and_preprocess(config, row0, col0, nrow=1, ncol=':'):
+def read_and_preprocess(config, row0, col0, nrow=1, ncol=':', out=None):
     """
 
     Note:
@@ -58,16 +62,25 @@ def read_and_preprocess(config, row0, col0, nrow=1, ncol=':'):
 
         arr.isel(x=0, y=0).dropna('time')
     """
+    # TODO: calculate windows elsewhere
+    windows = [((row0, row0 + nrow), (col0, col0 + ncol))]
+    # TODO: iterate over windows?
+    window = windows[0]
+
     datasets = {}
     for name, cfg in six.iteritems(config['data']['datasets']):
-        logger.debug('Reading "{}" dataset'.format(name))
-        arr = config_to_dataarray(cfg, row0, col0, nrow=nrow, ncol=ncol)
+        if cfg['reader'] in READER_BACKENDS:
+            reader = READER_BACKENDS[cfg['reader']](**cfg)
+            arr = reader.read_dataarray(window=window, out=out)
+        else:
+            raise KeyError('"{}" reader backend is not supported at this time'
+                           .format(cfg['reader']))
 
         if cfg['mask_band'] and cfg['mask_values']:
             logger.debug('Applying mask band to "{}"'.format(name))
             arr = apply_band_mask(arr, cfg['mask_band'], cfg['mask_values'])
 
-        # Min/Max values -- done here for now
+        # Min/Max values -- donberline here for now
         if cfg['min_values'] and cfg['max_values']:
             logger.debug('Applying range mask to "{}"'.format(name))
             arr = apply_range_mask(arr, cfg['min_values'], cfg['max_values'])
@@ -75,49 +88,6 @@ def read_and_preprocess(config, row0, col0, nrow=1, ncol=':'):
         datasets[name] = arr
 
     return merge_datasets(datasets)
-
-
-def config_to_dataarray(config, row0, col0, nrow=1, ncol=':'):
-    """ Turns a data configuration section into xarray.DataArray
-    """
-    df = _gdal.parse_dataset_file(config['input_file'], config['date_format'])
-
-    # TODO: get these from "driver" (GDAL / BIP / eventually, ARD connection)
-    md = dict()
-    with rasterio.drivers():
-        with rasterio.open(df['filename'][0]) as src:
-            md['affine'] = src.affine
-            md['crs'] = src.crs
-            md['nrow'] = src.height
-            md['ncol'] = src.width
-            md['vars'] = config['band_names']
-            md['dtypes'] = src.dtypes
-            assert len(config['band_names']) == src.count
-
-    nrow = md['nrow'] if nrow == ':' else nrow
-    ncol = md['ncol'] if ncol == ':' else ncol
-    window = ((row0, row0 + nrow), (col0, col0 + ncol))
-    nobs = len(df['date'])
-    nband = len(config['band_names'])
-    coord_y = md['affine'][2] + md['affine'][0] * np.arange(*window[0])
-    coord_x = md['affine'][5] + md['affine'][4] * np.arange(*window[1])
-
-    values = np.empty((nobs, nband, nrow, ncol), dtype=md['dtypes'][0])
-
-    # Read time series
-    # TODO: remove from here
-    with rasterio.drivers():
-        for i, row in df.iterrows():
-            with rasterio.open(row['filename']) as src:
-                src.read(window=window, out=values[i, ...], boundless=True)
-
-    # Create DataArray
-    da = xr.DataArray(
-        values,
-        dims=['time', 'band', 'y', 'x'],
-        coords=[df['date'], config['band_names'], coord_y, coord_x]
-    )
-    return da
 
 
 def merge_datasets(dataarrays):
