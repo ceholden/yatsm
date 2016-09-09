896af978a8ce588d905f2f5370e49464e47ef70b
Chris Holden <ceholden@gmail.com>
2016-09-09 02:35:46 -0400

    Add OLS-CUSUM, jit'd and tested vs strucchange

---
+++ tests/regression/test_structural_break.py
@@ -0,0 +1,84 @@
+""" Tests for yatsm.regression.structural_break
+"""
+import numpy as np
+import pandas as pd
+import pytest
+import xarray as xr
+
+try:
+    from rpy2 import robjects
+    from rpy2.robjects import pandas2ri
+    from rpy2.robjects.packages import importr
+    pandas2ri.activate()
+except ImportError:
+    has_rpy2 = False
+else:
+    has_rpy2 = True
+
+from yatsm.regression.structural_break import cusum_OLS
+
+
+@pytest.fixture('module')
+def test_data():
+    """ A very silly regression with an obvious change point
+    """
+    np.random.seed(0)
+    y = np.concatenate((np.random.rand(250), np.random.rand(250) + 1))
+    df = pd.DataFrame(np.column_stack((np.ones(500), np.arange(500), y)),
+                      columns=['int', 'slope', 'y'])
+
+    return df
+
+
+@pytest.fixture('module')
+def rpy2_strucchange():
+    if has_rpy2:
+        try:
+            utils = importr('utils')
+            utils.install_packages('strucchange')
+        except Exception as exc:
+            pytest.skip('Unable to install "strucchange"')
+        else:
+            return True
+    return False
+
+
+@pytest.fixture('module')
+def strucchange_cusum_OLS(rpy2_strucchange, test_data):
+    if has_rpy2 and rpy2_strucchange:
+        try:
+            rstr = """
+            function(dat) {
+                library(strucchange)
+                model <- efp(y ~ ., data=dat, type="OLS-CUSUM")
+                sctest(model)
+            }
+            """
+            rfunc = robjects.r(rstr)
+            rdat = robjects.r.ts(test_data)
+            result = rfunc(rdat)
+            result = dict(zip(result.names, list(result)))
+        except Exception as exc:
+            pass
+        else:
+            results = (result['statistic'][0], result['p.value'][0])
+            return results
+
+    # Validated against strucchange==1.5.1
+    return (3.706774647551639, 2.325251102774928e-12)
+
+
+def test_cusum_OLS(test_data, strucchange_cusum_OLS):
+    """ Tested against strucchange 1.5.1
+    """
+    y = test_data.pop('y')
+    X = test_data
+    # Test sending pandas
+    result = cusum_OLS(X, y)
+    assert np.allclose(result.score, strucchange_cusum_OLS[0])
+    assert np.allclose(result.pvalue, strucchange_cusum_OLS[1])
+
+    # And ndarray and xarray
+    result = cusum_OLS(X.values, xr.DataArray(y, dims=['time']))
+    assert np.allclose(result.score, strucchange_cusum_OLS[0])
+    assert np.allclose(result.pvalue, strucchange_cusum_OLS[1])
---
+++ yatsm/regression/structural_break.py
@@ -0,0 +1,78 @@
+# -*- coding: utf-8 -*-
+""" Methods for estimating structural breaks in time series regressions
+
+TODO: extract and move Chow test from "commission test" over to here
+"""
+from collections import namedtuple
+import logging
+
+import numpy as np
+import pandas as pd
+from scipy import stats
+import xarray as xr
+
+from ..accel import try_jit
+
+logger = logging.getLogger(__name__)
+
+pandas_like = (pd.DataFrame, pd.Series, xr.DataArray)
+
+# tuple: CUSUM-OLS results
+CUSUMOLSResult = namedtuple('CUSUMOLSResult', ['index', 'score', 'cusum',
+                                               'pvalue', 'signif'])
+
+# dict: CUSUM OLS critical values
+CUSUM_OLS_CRIT = {
+    0.01: 1.63,
+    0.05: 1.36,
+    0.10: 1.22
+}
+
+
+@try_jit(nopython=True, nogil=True)
+def _cusum_OLS(X, y):
+    n, p = X.shape
+    beta = np.linalg.lstsq(X, y)[0]
+    resid = np.dot(X, beta) - y
+
+    sigma = ((resid ** 2).sum() / (n - p) * n) ** 0.5
+
+    process = resid.cumsum() / sigma
+    _process = np.abs(process)
+    score = _process.max()
+    idx = _process.argmax()
+
+    return process, score, idx
+
+
+def cusum_OLS(X, y, alpha=0.05):
+    u""" CUSUM-OLS test for structural breaks
+
+    # TODO: same function for cusum_REC?
+
+    Args:
+        X (array like): 2D (n_features x n_obs) design matrix
+        y (array like): 1D (n_obs) indepdent variable
+        alpha (float): Test threshold (either 0.01, 0.05, or 0.10) from
+            Ploberger and Krämer (1992)
+
+    Returns:
+        tuple: the change point (index of ``y``), the test pvalue, and
+            a boolean testing if the CUSUM score is significant at the given
+            ``alpha``
+    """
+    _X = X.values if isinstance(X, pandas_like) else X
+    _y = y.values if isinstance(y, pandas_like) else y
+
+    cusum, score, idx = _cusum_OLS(_X, _y)
+    if isinstance(y, (pd.Series, pd.DataFrame)):
+        idx = y.index[idx]
+    elif isinstance(y, xr.DataArray):
+        idx = y.to_series().index[idx]
+
+    # crit = stats.kstwobign.isf(alpha)  ~70usec
+    crit = CUSUM_OLS_CRIT[alpha]
+    pval = stats.kstwobign.sf(score)
+
+    return CUSUMOLSResult(index=idx, score=score, cusum=cusum,
+                          pvalue=pval, signif=pval < crit)
