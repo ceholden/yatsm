d85e12aad3c2431de571405777b26b4f53ab6428
Chris Holden <ceholden@gmail.com>
2016-09-27 15:26:13 -0400

    Work toward pytables storage

--- yatsm/results/_pytables.py
+++ yatsm/results/_pytables.py
@@ -1,5 +1,9 @@
+""" Results storage in HDF5 datasets using PyTables
+"""
 import numpy as np
 import tables as tb
+
+SEGMENT_ATTRS = ['px', 'py', 'start', 'end', 'break']
 
 
 def dtype_to_table(dtype):
@@ -26,3 +30,37 @@ def dtype_to_table(dtype):
         _tb_dtype[name]._v_pos = idx
         desc.update(_tb_dtype)
     return desc
+
+
+def create_table(h5file, result, index=True):
+    """ Create table to store results
+
+    Args:
+        h5file (tables.file.File): PyTables HDF5 file
+        result (OrderedDict[str, np.ndarray]): Results as a NumPy
+            structured/record array, labeled according to the result type
+        index (bool): Create index on :ref:`SEGMENT_ATTRS`
+
+    Returns:
+        table.table.Table:
+    """
+    first = list(result.keys())[0]
+    for idx, (name, desc) in enumerate(result.items()):
+        if idx == 0:
+            table_desc = dtype_to_table(desc.dtype)
+        else:
+            table_desc.update({name: dtype_to_table(desc.dtype)})
+
+    table = h5file.create_table(h5file.root, first, table_desc)
+
+    if index:
+        for attr in SEGMENT_ATTRS:
+            getattr(table.col, attr).create_index()
+
+    return table
+
+
+class HDF5ResultsStore(object):
+    """ PyTables based HDF5 results storage
+    """
+    pass
