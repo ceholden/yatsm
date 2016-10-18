d2a4ddb5462d3628148ec1a0353e7bdc2a048472
Chris Holden <ceholden@gmail.com>
2016-10-18 09:55:38 -0400

    Add a JIT-able ddof stdev

--- tests/regression/test_diagnostics.py
+++ tests/regression/test_diagnostics.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from yatsm.regression.diagnostics import rmse
+from yatsm.regression.diagnostics import std, rmse
 
 n = 500
 
@@ -10,6 +10,16 @@ n = 500
 def y(prng):
     y = prng.standard_normal(n)
     return y
+
+
+@pytest.mark.parametrize('n', [100, 1000, 10000])
+def test_std(n):
+    np.random.seed(42)
+    x = np.random.normal(size=n)
+    for ddof in (1, 2, 3, 4):
+        result = std(x, ddof=ddof)
+        expected = np.std(x, ddof=ddof)
+        assert abs(result - expected) < 1e-4
 
 
 def test_rmse(y):
--- yatsm/regression/diagnostics.py
+++ yatsm/regression/diagnostics.py
@@ -8,7 +8,24 @@ import numpy as np
 from ..accel import try_jit
 
 
-@try_jit(nopython=True)
+#@try_jit(nopython=True, nogil=True)
+def std(x, ddof=1):
+    """ JIT-able implementation of standard deviation with degress of freedom
+
+    Args:
+        x (np.ndarray): Array of values
+        ddof (int): Delta degrees of freedom. The divisor used in calculations
+            is ``N - ddof`` where ``N`` is the number of elements in ``x``.
+
+    Returns:
+        float: The standard deviation
+    """
+    n = x.shape[0]
+    m = x.sum() / (n - ddof)
+    return (((x - m) ** 2).sum() / (n - ddof)) ** 0.5
+
+
+@try_jit(nopython=True, nogil=True)
 def rmse(y, yhat):
     """ Calculate and return Root Mean Squared Error (RMSE)
 
