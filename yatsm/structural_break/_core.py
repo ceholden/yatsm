eea9cabf7f15106124b3eb2b69f050f63f4c16f2
Chris Holden <ceholden@gmail.com>
2016-10-20 11:42:18 -0400

    Fix struc break named tuple name

--- yatsm/structural_break/_core.py
+++ yatsm/structural_break/_core.py
@@ -8,4 +8,4 @@ pandas_like = (pd.DataFrame, pd.Series, xr.DataArray)
 _fields = ['method', 'index', 'score', 'process', 'pvalue', 'signif']
 
 #: namedtuple: Structural break detection results
-StructuralBreakResult = namedtuple('StructuralBreak', _fields)
+StructuralBreakResult = namedtuple('StructuralBreakResult', _fields)
