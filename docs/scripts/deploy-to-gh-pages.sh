13f935f6974f7524b3b301c92cbea5bba779bd95
Chris Holden <ceholden@gmail.com>
2017-01-19 01:46:56 -0500

    Revise GHP docs deploy script to be multi-version

--- docs/scripts/deploy-to-gh-pages.sh
+++ docs/scripts/deploy-to-gh-pages.sh
@@ -4,25 +4,34 @@
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-    echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-    exit 0
-fi
-rev=$(git rev-parse --short HEAD)
+DST_BRANCH=gh-pages
 
-cd $(dirname $(readlink -f $0))/../
-
-rm -rf build/
-
-make html
-cd build/html/
-
-git init
 git config user.email "ceholden@gmail.com"
 git config user.name "Chris Holden"
-git remote add upstream "https://${GITHUB_TOKEN}@${GITHUB_REPO}"
-git fetch upstream
-git reset upstream/gh-pages
+
+# if [ "$TRAVIS_BRANCH" != "master" ]; then
+#     echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+#     exit 0
+# fi
+rev=$(git rev-parse --short HEAD)
+SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+docs=$(dirname $(readlink -f $0))/../
+cd $docs
+
+rm -rf build
+
+mkdir -p yatsm_docs/$SRC_BRANCH && cd yatsm_docs
+git init
+git remote add origin "https://${GITHUB_TOKEN}@${GITHUB_REPO}"
+git fetch origin $DST_BRANCH
+git checkout $DST_BRANCH
+cd ..
+
+make html
+cp -R build/html/* yatsm_docs/$SRC_BRANCH/
+
+cd yatsm_docs/
 git add -A .
-git commit -m "Rebuild gh-pages docs at ${rev}"
-git push --quiet upstream HEAD:gh-pages
+git commit -m "Rebuild $DST_BRANCH docs on ${SRC_BRANCH} at ${rev}"
+git push --quiet origin HEAD:$DST_BRANCH
