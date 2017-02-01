6289465a061d8ecea65fb23abebf2a922fea79c0
Chris Holden <ceholden@gmail.com>
2017-02-01 14:19:19 -0500

    Try new way of deploying docs

--- docs/scripts/deploy-to-gh-pages.sh
+++ docs/scripts/deploy-to-gh-pages.sh
@@ -4,26 +4,43 @@
 
 set -o errexit -o nounset
 
+KEY_FILE=.deploy_key
+
+REPO=$(git config remote.origin.url)
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+REV=$(git rev-parse --short HEAD)
+
+SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 DST_BRANCH=gh-pages
 
-git config user.email "ceholden@gmail.com"
-git config user.name "Chris Holden"
+# CONFIGURE GIT
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc \
+    -K $ENCRYPTED_KEY \
+    -iv $ENCRYPTED_IV \
+    -in ${KEY_FILE}.enc \
+    -out ${KEY_FILE} \
+    -d
+chmod 600 ${KEY_FILE}
+eval `ssh-agent -s`
+ssh-add ${KEY_FILE}
 
-# if [ "$TRAVIS_BRANCH" != "master" ]; then
-#     echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-#     exit 0
-# fi
-rev=$(git rev-parse --short HEAD)
-SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+git config user.email $COMMIT_AUTHOR_EMAIL
+git config user.name $COMMIT_AUTHOR_NAME
 
+# Find and clean
 docs=$(dirname $(readlink -f $0))/../
 cd $docs
 
 rm -rf build
 
+# Create branch directory and grab Git repo
 mkdir -p yatsm_docs/$SRC_BRANCH && cd yatsm_docs
 git init
-git remote add origin "https://${GITHUB_TOKEN}@${GITHUB_REPO}"
+git remote add origin $SSH_REPO
 git fetch origin $DST_BRANCH
 git checkout $DST_BRANCH
 cd ..
@@ -33,5 +50,5 @@ cp -R build/html/* yatsm_docs/$SRC_BRANCH/
 
 cd yatsm_docs/
 git add -A .
-git commit -m "Rebuild $DST_BRANCH docs on ${SRC_BRANCH} at ${rev}"
+git commit -m "Rebuild $DST_BRANCH docs on ${SRC_BRANCH} at ${REV}"
 git push --quiet origin HEAD:$DST_BRANCH
