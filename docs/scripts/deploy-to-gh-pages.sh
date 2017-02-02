7a3f5bda466e2e65c35e09676b5d9ca5bfdf50f6
Chris Holden <ceholden@gmail.com>
2017-02-02 13:41:18 -0500

    Use ssh url on Travis for docs & fix deploy

--- docs/scripts/deploy-to-gh-pages.sh
+++ docs/scripts/deploy-to-gh-pages.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 # Based off instructions from:
-# http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/
+# https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 
 set -o errexit -o nounset
 
 KEY_FILE=.deploy_key
+SOURCE=_build
 
 REPO=$(git config remote.origin.url)
 SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
 REV=$(git rev-parse --short HEAD)
 
-SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+SRC_BRANCH=$TRAVIS_BRANCH
 DST_BRANCH=gh-pages
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+	echo "Not building docs for PR"
+	exit 0
+fi
+echo "Building docs for branch: $SRC_BRANCH"
 
 # CONFIGURE GIT
 ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
@@ -19,17 +26,17 @@ ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
 ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
 ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
 openssl aes-256-cbc \
-    -K $ENCRYPTED_KEY \
-    -iv $ENCRYPTED_IV \
-    -in ${KEY_FILE}.enc \
-    -out ${KEY_FILE} \
-    -d
+        -K $ENCRYPTED_KEY \
+        -iv $ENCRYPTED_IV \
+        -in ${KEY_FILE}.enc \
+        -out ${KEY_FILE} \
+        -d
 chmod 600 ${KEY_FILE}
 eval `ssh-agent -s`
 ssh-add ${KEY_FILE}
 
-git config user.email $COMMIT_AUTHOR_EMAIL
-git config user.name $COMMIT_AUTHOR_NAME
+git config --global user.email $COMMIT_AUTHOR_EMAIL
+git config --global user.name $COMMIT_AUTHOR_NAME
 
 # Find and clean
 docs=$(dirname $(readlink -f $0))/../
@@ -38,17 +45,16 @@ cd $docs
 rm -rf build
 
 # Create branch directory and grab Git repo
-mkdir -p yatsm_docs/$SRC_BRANCH && cd yatsm_docs
-git init
-git remote add origin $SSH_REPO
-git fetch origin $DST_BRANCH
-git checkout $DST_BRANCH
-cd ..
+git clone $SSH_REPO $SOURCE/
+cd $SOURCE/
+git checkout $DST_BRANCH || git checkout --orphan $DST_BRANCH
+cd $docs
 
 make html
-cp -R build/html/* yatsm_docs/$SRC_BRANCH/
+mkdir -p $SOURCE/$SRC_BRANCH/
+cp -R build/html/* $SOURCE/$SRC_BRANCH/
 
-cd yatsm_docs/
+cd $SOURCE/
 git add -A .
-git commit -m "Rebuild $DST_BRANCH docs on ${SRC_BRANCH} at ${REV}"
-git push --quiet origin HEAD:$DST_BRANCH
+git commit -m "Rebuild $DST_BRANCH docs on $SRC_BRANCH: ${REV}"
+git push origin HEAD:$DST_BRANCH
