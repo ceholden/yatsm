6a0d59ca0594792e9a5b8e7ccdd8c989e2e551c0
Chris Holden <ceholden@gmail.com>
2017-02-06 14:05:53 -0500

    Update existing tasks docs/paradigm

--- yatsm/pipeline/tasks/change.py
+++ yatsm/pipeline/tasks/change.py
@@ -1,28 +1,42 @@
 """ Functional wrappers around change detection algorithms
 """
-import patsy
-
 from yatsm.algorithms import CCDCesque
 from .._task_validation import outputs, requires
 
 
 @requires(data=[])
 @outputs(record=[str])
-def pixel_CCDCesque(work, require, output, config=None):
-    """ Run CCDCesque on a pixel
+def pixel_CCDCesque(pipe, require, output, config=None):
+    """ Run :class:`yatsm.algorithms.CCDCesque` on a pixel
+
+    Users should pass to ``require`` both ``X`` and ``Y`` arguments, which
+    are interpreted as:
+
+    .. code-block:: python
+
+        X, Y = require[0], require[1:]
+
+    Args:
+        pipe (yatsm.pipeline.Pipe): Piped data to operate on
+        require (dict[str, list[str]]): Labels for the requirements of this
+            calculation
+        output (dict[str, list[str]]): Label for the result of this
+            calculation
+        config (dict): Configuration to pass to :class:`CCDCesque`. Should
+            contain `init` sections
+
+    Returns:
+        yatsm.pipeline.Pipe: Piped output
+
     """
-    arr = work['data'][require['data']].dropna('time', how='any').to_array()
+    XY = pipe.data[require['data']].dropna('time', how='any')
+    X = XY[require['data'][0]]
+    Y = XY[require['data'][1:]].to_array()
 
     model = CCDCesque(**config.get('init', {}))
-    model.py, model.px = arr.y, arr.x
+    model.py, model.px = Y.y, Y.x
 
-    ordinal = arr.indexes['time'].map(lambda x: x.toordinal())
-    design = config.get('fit', {}).get('design', '1 + ordinal')
-    X = patsy.dmatrix(design,
-                      data=arr,
-                      eval_env=patsy.EvalEnvironment.capture())
+    model = model.fit(X, Y.values, XY['ordinal'])
+    pipe.record[output['record'][0]] = model.record
 
-    model = model.fit(X, arr.values, ordinal)
-    work['record'][output['record'][0]] = model.record
-
-    return work
+    return pipe
--- yatsm/pipeline/tasks/preprocess.py
+++ yatsm/pipeline/tasks/preprocess.py
@@ -6,8 +6,8 @@ import numpy as np
 import patsy
 import xarray as xr
 
-from .._task_validation import eager_task, requires, outputs
-from ...regression.transforms import harm
+from yatsm.pipeline._task_validation import eager_task, requires, outputs
+from yatsm.regression.transforms import harm
 
 logger = logging.getLogger(__name__)
 
@@ -15,37 +15,47 @@ logger = logging.getLogger(__name__)
 @eager_task
 @requires(data=[str, str])
 @outputs(data=[str])
-def norm_diff(work, require, output, config=None):
+def norm_diff(pipe, require, output, config=None):
     """ Calculate a normalized difference of two bands
 
     Args:
-        work: Dataset to operate on
+        pipe (yatsm.pipeline.Pipe): Piped data to operate on
         require (dict[str, list[str]]): Labels for the requirements of this
             calculation
-        output (dict[str, list[str]]): Labels for the result of this
+        output (dict[str, list[str]]): Label for the result of this
             calculation
 
     Returns:
-        dict: Input ``work`` dictionary with the calculation added according
-            to user specification of ``output``
+        yatsm.pipeline.Pipe: Piped output
     """
     one, two = require['data']
     out = output['data'][0]
 
-    work['data'][out] = ((work['data'][one] - work['data'][two]) /
-                         (work['data'][one] + work['data'][two]))
+    pipe.data[out] = ((pipe.data[one] - pipe.data[two]) /
+                      (pipe.data[one] + pipe.data[two]))
 
-    return work
+    return pipe
 
 
 @eager_task
 @requires(data=[])
 @outputs(data=[str])
-def dmatrix(work, require, output, config=None):
+def dmatrix(pipe, require, output, config=None):
     """ Create a design matrix from Patsy/R style design strings
+
+    Args:
+        pipe (yatsm.pipeline.Pipe): Piped data to operate on
+        require (dict[str, list[str]]): Labels for the requirements of this
+            calculation
+        output (dict[str, list[str]]): Label for the result of this
+            calculation
+        config
+
+    Returns:
+        yatsm.pipeline.Pipe: Piped output
     """
     design = config['design']
-    ds = work['data']
+    ds = pipe.data
 
     if '~' in design:
         X = patsy.dmatrices(design, ds)
@@ -57,6 +67,6 @@ def dmatrix(work, require, output, config=None):
 
     coords = (ds['time'], X.design_info.column_names)
     dims = ('time', 'terms')
-    work['data'][output['data'][0]] = xr.DataArray(X, coords, dims)
+    pipe.data[output['data'][0]] = xr.DataArray(X, coords, dims)
 
-    return work
+    return pipe
