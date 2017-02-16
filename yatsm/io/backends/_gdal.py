Fix coordinate calculation :sweat_smile:

yatsm/io/backends/_gdal.py
@@ -254,8 +254,12 @@ class GDALTimeSeries(object):
         Returns:
             tuple (np.ndarray, np.ndarray): Y and X coordinates for window
         """
-        coord_y = self.ul[0] + self.res[0] * np.arange(*window[0])
-        coord_x = self.ul[1] + self.res[1] * np.arange(*window[1])
+        x0, y0 = self.ul[0], self.ul[1]
+        nx, ny = window[1][1] - window[1][0], window[0][1] - window[0][0]
+        dx, dy = self.res[0], -self.res[1]
+
+        coord_x = np.linspace(start=x0, num=nx, stop=(x0 + (nx - 1) * dx))
+        coord_y = np.linspace(start=y0, num=ny, stop=(y0 + (ny - 1) * dy))
 
         return (coord_y, coord_x)
 