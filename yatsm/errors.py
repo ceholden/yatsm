f5a14f69c28d8cf642269a3388d75ecebb844569
Chris Holden <ceholden@gmail.com>
2017-02-27 14:32:53 -0500

    Add new error and docs info

--- yatsm/errors.py
+++ yatsm/errors.py
@@ -1,3 +1,6 @@
+from yatsm.version import __docs_version__
+
+
 class TSLengthException(Exception):
     """ Exception stating timeseries does not contain enough observations
     """
@@ -22,3 +25,18 @@ class PipelineConfigurationError(TypeError):
     """ Exception for invalid ``require``/``output`` specification in pipeline
     """
     pass
+
+
+class PipelineConfigurationNotFound(KeyError):
+    """ Exception for uses requiring missing information
+
+    Provides help message with link to configuration docs.
+    """
+    # TODO: finish help message & docs
+    _help = ('Missing configuration information required to accomplish task. '
+             'Please consult {0}/{1} for more information'
+             .format(__docs_version__, 'guide/pipeline.html#errors'))
+
+    def __init__(self, msg, *args, **kwds):
+        msg = '{0} : "{1}"'.format(self._help, msg)
+        super(KeyError, self).__init__(msg, *args[1:], **kwds)
--- yatsm/version.py
+++ yatsm/version.py
@@ -1,1 +1,4 @@
 __version__ = '0.7.dev0'
+
+__docs__ = 'https://ceholden.github.io/yatsm/'
+__docs_version__ = '%s/%s' % (__docs__, __version__)
