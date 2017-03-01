a1042e8ab2229e2ad3a0c6461bed3cb4579259dc
Chris Holden <ceholden@gmail.com>
2017-03-01 15:47:03 -0500

    Cleanup unused code

--- yatsm/regression/__init__.py
+++ yatsm/regression/__init__.py
@@ -1,13 +1,11 @@
 from ._recresid import recresid
 from .design import design_coefs, design_to_indices
-from .packaged import find_packaged_regressor
 from .robust_fit import RLM, bisquare
 from .transforms import harm
 
 __all__ = [
     'design_coefs',
     'design_to_indices',
-    'find_packaged_regressor',
     'recresid',
     'RLM',
     'bisquare',
