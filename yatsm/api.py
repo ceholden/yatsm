ae07c41c5b03f0f053ad0deb6b84a9286a5c77e8
Chris Holden <ceholden@gmail.com>
2017-03-10 18:01:28 -0500

    Add table investigation methods

--- yatsm/api.py
+++ yatsm/api.py
@@ -2,6 +2,8 @@
 """
 import copy
 import functools
+import itertools
+import logging
 
 from yatsm.config import validate_and_parse_configfile
 from yatsm.errors import PipelineConfigurationNotFound
@@ -10,6 +12,8 @@ from yatsm.pipeline import Pipe, Pipeline
 from yatsm.results import HDF5ResultsStore
 from yatsm.results.utils import pattern_to_regex as _pattern_to_regex
 from yatsm.utils import cached_property as _cached_property, find as _find
+
+logger = logging.getLogger(__name__)
 
 
 def _checked_property(prop):
@@ -77,6 +81,61 @@ class Config(object):  # TODO: rename?
         results = _find(output, pattern, regex=True)
         for result in results:
             yield HDF5ResultsStore(result, **kwds)
+
+    def peak_results(self, results, table=None):
+        """ Pop an example result file off a list to inspect
+
+        Args:
+            results (iterable): A generator or list of
+                :class:`HDF5ResultsStore`
+            table (str): Optionally, table to require in order to be a "valid"
+                example
+
+        Returns:
+            tuple: Return an example :class:`HDF5ResultsStore` and the original
+            generator (or list) supplied to the function (with the example
+            still in the generator)
+
+        Raises:
+            StopIteration: Raise (by not catching if thrown) if function cannot
+            find any valid results
+        """
+        result = None
+        _results = []  # storage for iterator's items we've seen
+        while result is None:
+            candidate = next(results)
+            _results.append(candidate)  # put back
+
+            try:
+                tables = list(candidate.tables())
+                if not tables:
+                    logger.debug('No results to peak in {0}'
+                                 .format(candidate.filename))
+                elif table and table not in tables:
+                    logger.debug('Table "{0}" not found in file {1}'
+                                 .format(table, candidate.filename))
+                else:
+                    result = candidate
+            except Exception as exc:
+                logger.debug('Could not use "{0}" as example result file: '
+                             '{1!r}'.format(candidate.filename, exc))
+
+        return result, itertools.chain(_results, results)
+
+    def peak_table(self, store):
+        """ Return the first obvious table to use from a HDF5ResultsStore
+
+        Args:
+            store (HDF5ResultsStore): HDF5 results file
+
+        Returns:
+            str or None: table location, or `None` if there are no tables
+            stored
+        """
+        tables = list(store.tables())
+        if tables:
+            table_name = tables[0][0]
+            return table_name
 
     # PROPERTY ACCESS
     @_checked_property
