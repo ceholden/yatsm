c4a99316543a25efec5575bf9e7cbe9578556d7f
Chris Holden <ceholden@gmail.com>
2017-03-16 16:52:38 -0400

    Add a custom TODO exception

--- yatsm/errors.py
+++ yatsm/errors.py
@@ -1,4 +1,6 @@
-from yatsm.version import __docs_version__
+from yatsm.version import (__docs_version__,
+                           __repo_issues__,
+                           __version__)
 
 
 class TSLengthException(Exception):
@@ -40,3 +42,16 @@ class PipelineConfigurationNotFound(KeyError):
     def __init__(self, msg, *args, **kwds):
         msg = '{0} : "{1}"'.format(self._help, msg)
         super(KeyError, self).__init__(msg, *args[1:], **kwds)
+
+
+class TODO(NotImplementedError):
+
+    _help = ('This version ("{version}") has not yet implemented this '
+             'functionality. Please visit {issues} and copy and paste '
+             'the following message if you would like this feature to be '
+             'supported.'
+             .format(version=__version__, issues=__repo_issues__))
+
+    def __init__(self, msg, *args, **kwds):
+        msg = '{0} : "{1}"'.format(self._help, msg)
+        super(NotImplementedError, self).__init__(msg, *args[1:], **kwds)
--- yatsm/version.py
+++ yatsm/version.py
@@ -2,3 +2,5 @@ __version__ = '0.7.dev0'
 
 __docs__ = 'https://ceholden.github.io/yatsm/'
 __docs_version__ = '%s/%s' % (__docs__, __version__)
+__repo_url__ = 'https://github.com/ceholden/yatsm'
+__repo_issues__ = '/'.join([__repo_url__, 'issues'])
