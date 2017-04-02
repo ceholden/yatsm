490af79a135851ed9e3d83c005af327c29419336
Chris Holden <ceholden@gmail.com>
2017-04-02 19:15:58 -0400

    Fix tests

--- tests/config/test_config.py
+++ tests/config/test_config.py
@@ -3,18 +3,16 @@
 import pytest
 import yaml
 
-from yatsm.config import parse_config
+from yatsm.config import (load_config,
+                          parse_config,
+                          validate_config)
 from yatsm.errors import InvalidConfigurationException
 
 
 @pytest.mark.parametrize(('cfg', 'name'), [
     ({}, 'version'),
 ])
-def test_parse_config_missing_required(tmpdir, cfg, name):
-    fn = tmpdir.join('cfg.yml').strpath
-    with open(fn, 'w') as fid:
-        fid.write(yaml.dump(cfg))
-
+def test_validate_config_missing_required(cfg, name):
     with pytest.raises(InvalidConfigurationException) as err:
-        parse_config(fn)
+        validate_config(cfg)
     assert name in str(err) and 'is a required property' in str(err)
