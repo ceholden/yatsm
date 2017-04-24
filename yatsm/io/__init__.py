ad18ee9eec8a92fb4c48605cb74785c28de384a9
Chris Holden <ceholden@gmail.com>
2017-04-24 17:30:35 -0400

    Publicize xarray functions

--- yatsm/io/__init__.py
+++ yatsm/io/__init__.py
@@ -1,18 +1,18 @@
 """ YATSM IO module
 
-.. todo::
-
-   Include result file IO abstraction (:issue:`69`)
-
 Contents:
 
     * :mod:`._util`: Collection of helper functions that ease common
       filesystem operations
 """
 from ._api import get_readers, read_and_preprocess
+from ._xarray import apply_band_mask, apply_range_mask, merge_data
 
 
 __all__ = [
     'get_readers',
-    'read_and_preproces'
+    'read_and_preproces',
+    'apply_band_mask',
+    'apply_range_mask',
+    'merge_data'
 ]
