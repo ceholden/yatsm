2d47dc640f6a154a4c4779a460f9a8ff3436fbef
Chris Holden <ceholden@gmail.com>
2017-05-01 15:05:00 -0400

    Add scale factor for EVI calculations
    Good for data scaled by e.g., 10,000

--- yatsm/vegetation_indices.py
+++ yatsm/vegetation_indices.py
@@ -3,31 +3,41 @@
 from __future__ import division
 
 
-def EVI(red, nir, blue):
+def EVI(red, nir, blue, scale_factor=1.0):
     """ Return the Enhanced Vegetation Index for a set of np.ndarrays
 
     EVI is calculated as:
 
     .. math::
-        2.5 * \\frac{(NIR - RED)}{(NIR + C_1 * RED - C_2 * BLUE + L)}
+        G * \\frac{(NIR - RED)}{(NIR + C_1 * RED - C_2 * BLUE + L)}
 
     where:
         - :math:`RED` is the red band
         - :math:`NIR` is the near infrared band
         - :math:`BLUE` is the blue band
+        - :math:`G = 2.5` is the gain factor
         - :math:`C_1 = 6`
         - :math:`C_2 = 7.5`
         - :math:`L = 1`
 
-    Note: bands must be given in float datatype from [0, 1]
+    Note: bands must be given in float datatype from [0, 1]. If not,
+          please provide the appropriate `scale_factor` to multiply
+          by. This correction will be applied to the `L` parameter.
 
     Args:
-      red (np.ndarray): red band
-      nir (np.ndarray): NIR band
-      blue (np.ndarray): blue band
+        red (np.ndarray): red band
+        nir (np.ndarray): NIR band
+        blue (np.ndarray): blue band
+        scale_factor (float): Scaling factor for data (e.g., `10000` for
+            scaled by 10,000)
 
     Returns:
-      np.ndarray: EVI
+        np.ndarray: EVI
 
     """
-    return 2.5 * (nir - red) / (nir + 6 * red - 7.5 * blue + 1)
+    G = 2.5
+    C_1 = 6
+    C_2 = 7.5
+    L = 1.0 * scale_factor
+
+    return G * (nir - red) / (nir + C_1 * red - C_2 * blue + L)
