76aa4b2ce1e78f864cfe5f8b4924cfcd5f32494c
Chris Holden <ceholden@gmail.com>
2017-07-25 10:35:20 -0400

    Update master docs build for ghpages

--- docs/scripts/deploy-to-gh-pages.sh
+++ docs/scripts/deploy-to-gh-pages.sh
@@ -1,28 +1,77 @@
 #!/bin/bash
 # Based off instructions from:
-# http://www.steveklabnik.com/automatically_update_github_pages_with_travis_example/
+# https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
 
 set -o errexit -o nounset
 
-if [ "$TRAVIS_BRANCH" != "master" ]; then
-    echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
-    exit 0
+PACKAGE="yatsm"
+
+DOCS=$(dirname $(readlink -f $0))/../
+APIDOC="${DOCS}/source/${PACKAGE}"
+
+KEY_FILE=.deploy_key
+SOURCE=_build
+
+REPO=$(git config remote.origin.url)
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+REV=$(git rev-parse --short HEAD)
+
+# SRC_BRANCH=$TRAVIS_BRANCH
+SRC_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+DST_BRANCH=gh-pages
+
+set +u
+if [ "$TRAVIS" == "true" ]; then
+    set -u
+    if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+        echo "Not building docs for PR"
+        exit 0
+    fi
+
+    # CONFIGURE GIT
+    ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+    ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+    ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+    ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+    openssl aes-256-cbc \
+            -K $ENCRYPTED_KEY \
+            -iv $ENCRYPTED_IV \
+            -in ${KEY_FILE}.enc \
+            -out ${KEY_FILE} \
+            -d
+    chmod 600 ${KEY_FILE}
+    eval `ssh-agent -s`
+    ssh-add ${KEY_FILE}
+
+    git config --global user.email $COMMIT_AUTHOR_EMAIL
+    git config --global user.name $COMMIT_AUTHOR_NAME
 fi
-rev=$(git rev-parse --short HEAD)
+set -u
 
-cd $(dirname $(readlink -f $0))/../
+echo "Building docs for branch: $SRC_BRANCH"
 
-rm -rf _build/
+# START
+cd $DOCS/
+
+# Clean
+rm -rf build
+rm -rf $SOURCE
+
+# Create branch directory and grab Git repo
+git clone $SSH_REPO $SOURCE/
+cd $SOURCE/
+git checkout $DST_BRANCH || git checkout --orphan $DST_BRANCH
+cd $DOCS/
+rm -rf $SOURCE/**/* || exit 0
+
+sphinx-apidoc -f -e -o $APIDOC ../${PACKAGE}/
 
 make html
-cd _build/html/
+rm -rf $SOURCE/$SRC_BRANCH/
+mkdir -p $SOURCE/$SRC_BRANCH/
+cp -R build/html/* $SOURCE/$SRC_BRANCH/
 
-git init
-git config user.email "ceholden@gmail.com"
-git config user.name "Chris Holden"
-git remote add upstream "https://${GITHUB_TOKEN}@${GITHUB_REPO}"
-git fetch upstream
-git reset upstream/gh-pages
+cd $SOURCE/
 git add -A .
-git commit -m "Rebuild gh-pages docs at ${rev}"
-git push --quiet upstream HEAD:gh-pages
+git commit -m "Rebuild $DST_BRANCH docs on $SRC_BRANCH: ${REV}"
+git push origin HEAD:$DST_BRANCH
